065652442a8fdcba134e09a493fb1ff1f1e4193d
itkq <kuogsi@gmail.com>
2016-12-04 19:46:34 +0900

    :sparkles: Use unix domain socket

--- main.go
+++ main.go
@@ -7,8 +7,14 @@ import (
 	"aista-search/session"
 	"aista-search/view"
 	"aista-search/view/plugin"
+	"fmt"
+	"log"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
 	"runtime"
+	"syscall"
 )
 
 func init() {
@@ -29,5 +35,26 @@ func main() {
 	)
 
 	router := route.New()
-	http.ListenAndServe(config.GetEnv("APP_PORT", ":8080"), router)
+	sock := "/tmp/app.sock"
+	os.Remove(sock)
+	l, err := net.Listen("unix", sock)
+	if err != nil {
+		fmt.Println("%s\n", err)
+		return
+	}
+	os.Chmod(sock, 0777)
+
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, os.Interrupt, os.Kill, syscall.SIGTERM)
+	go func(c chan os.Signal) {
+		sig := <-c
+		log.Printf("Caught signal %s: shutting down", sig)
+		l.Close()
+		os.Exit(0)
+	}(sigc)
+
+	err = http.Serve(l, router)
+	if err != nil {
+		panic(err)
+	}
 }
